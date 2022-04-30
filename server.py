d64ca5845a1c1d5aaced1b73b025ee4062b55a21
SawshaDev <contactcallimarie@gmail.com>
2022-04-30 02:24:37 +0000

    dont ask.

--- server.py
+++ server.py
@@ -2,10 +2,14 @@
 import praw
 import random
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, send_from_directory, url_for
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 reddit = praw.Reddit(client_id='pua14mlzkyv5_ZfQ2WmqpQ',
                      client_secret='T5loHbaVD7m-RNMpFf0z24iOJsInwg',
                      user_agent='Oxygen',
@@ -23,7 +27,7 @@ def favicon():
 @app.route("/", methods=['GET'])
 def home():
     
-    subreddits = ['FemboysAndHentai', 'hentai', 'rule34', 'nsfw']
+    subreddits = ['hentai', 'rule34', 'nsfw']
     choices = random.choice(subreddits)
     nsfw_submission = reddit.subreddit(choices).new(limit=15)
     post_to_pick = random.randint(1,15)
@@ -43,6 +47,25 @@ def home():
 
 
     return render_template("home.html", title=title, url=url, upvotes=upvotes, author=author, upvote_ratio=upvote_ratio, link=link)
+
+@app.route('/hentai')
+def hentai():
+    nsfw_submission = reddit.subreddit("hentai").new(limit=15)
+    post_to_pick = random.randint(1,15)
+    for i in range(0, post_to_pick):
+            submission = next(x for x in nsfw_submission if not x.stickied)
+
+    
+    title = submission.title
+    link = submission.permalink
+    url = submission.url
+
+    upvotes = submission.score
+    upvote_ratio = submission.upvote_ratio
+    author = submission.author
+    
+    return f"<img src='{url}'>"
+
 
 @app.route("/endpoint")
 def endpoints():
@@ -65,17 +88,6 @@ def femboy():
     return {'status': 200, 'title':title,'link': submission.url}
 
 
-@app.route("/api/v1/nsfw")
-def nsfw():
-    nsfw_submission = reddit.subreddit('nsfw').hot()
-    post_to_pick = random.randint(1,30)
-    for i in range(0, post_to_pick):
-        if random_sub.is_reddit_media_domain and not random_sub.is_video:
-        	submission = next(x for x in nsfw_submission if not x.stickied)
-
-    title = submission.title
-
-    return {'status':200, 'title':title, 'link':submission.url}
 
 
 
